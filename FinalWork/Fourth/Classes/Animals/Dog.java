b2023d93824d0150197897098a252ff95a00791a
Dmitri <datitov2009@yandex.ru>
2023-06-14 00:39:38 +0300

    Add folder fourth task, included programm on java

---
+++ FinalWork/Fourth/Classes/Animals/Dog.java
@@ -0,0 +1,11 @@
+package FinalWork.Fourth.Classes.Animals;
+
+import FinalWork.Fourth.Classes.Pets;
+
+public class Dog extends Pets {
+
+    public Dog(String name) {
+        super(name);
+        super.setKindAnimal("Dog");
+    }
+}
